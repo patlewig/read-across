02df92ef5c8be8fea59c9c82cb5c2de87f964020
patlewig <patlewig@hotmail.com>
2023-12-14 18:43:01 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repository supporting manuscript entitled A systematic analysis of read-across within REACH registration dossiers. ',
+    author='patlewig',
+    license='MIT',
+)
